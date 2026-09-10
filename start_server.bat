a779bddd1d5ffe77e43322158c71ffb4906beed1
SIVABALAN <balansiva02121997@gmail.com>
2026-09-10 17:52:40 +0530

    Run secure application by default

--- start_server.bat
+++ start_server.bat
@@ -1,7 +1,4 @@
 @echo off
 cd /d "%~dp0"
-if not exist .venv python -m venv .venv
-call .venv\Scripts\activate.bat
-python -m pip install -r requirements.txt
-python app.py
+python app_secure.py
 pause
